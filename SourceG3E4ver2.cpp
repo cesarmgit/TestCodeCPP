3a13d3401db3c889699c4b82898eea4a75740c54
Cesar <cesarm.movil@gmail.com>
2024-05-16 02:06:51 -0300

    another version

---
+++ SourceG3E4ver2.cpp
@@ -0,0 +1,29 @@
+/*Hacer un programa para que el usuario ingrese un número positivo y que luego
+se muestre por pantalla los números entre el 1 y el número ingresado por el usuario.
+Ejemplo. Si el usuario ingresa 15, se mostrarán los números entre el 1 y el 15.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int nroIngresado;
+
+    cout << "Ingrese número:" << endl;
+    cin >> nroIngresado;
+    cout << endl;
+
+    for( int i=1 ; i<=nroIngresado ; i++ ) {
+        if (i == nroIngresado){;
+            cout << i;
+        }
+        else {
+            cout << i << " - ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
